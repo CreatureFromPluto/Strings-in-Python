b29335068afca21f6f012bd342c660d2dee2cb8f
CreatureFromPluto <ankit.sinha776@gmail.com>
2020-10-16 23:43:08 +0530

    This is my first commit message

---
+++ 5.py
@@ -0,0 +1,13 @@
+# Question 5
+
+str = 'beautiful'
+n = 2
+result = str[-2:] + str[:-2]
+print(result)
+n = 9
+result2 = str[-9:] + str[:-9]
+print(result2)
+n = 13
+k = 13%9
+result3 = str[-k:] + str[:-k]
+print(result3)
